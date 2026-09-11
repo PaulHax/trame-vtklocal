65962a1638d1416f336375c901cf2ad23299f3fe
Paul Elliott <paul@vizworkshop.com>
2026-09-11 08:57:59 -0400

    Revert "perf: suspend streamed members in hidden views"
    This reverts commit ef06bd25e4e57e6b02605dcd3c4cfac280b916c8.

--- vue-components/src/components/VtkJsLocal.js
+++ vue-components/src/components/VtkJsLocal.js
@@ -14,7 +14,6 @@ import { bindDistanceToCameraInteractorRenderEvent } from "./distanceToCameraGly
 import { createViewApi, VIEW_EMITS, VIEW_PROPS } from "./viewApi";
 import { registerView, unregisterView } from "./viewRegistry";
 import { getDevicePixelRatio } from "./viewportMetrics";
-import { observeElementVisibility } from "./elementVisibility";
 
 export default {
   emits: VIEW_EMITS,
@@ -28,7 +27,6 @@ export default {
     let renderWindow = null;
     let interactor = null;
     let resizeObserver = null;
-    let visibilityObserver = null;
     let interactorRenderSubscription = null;
     let cameraSubscriptions = [];
 
@@ -121,10 +119,6 @@ export default {
 
       resizeObserver = new ResizeObserver(resize);
       resizeObserver.observe(container.value);
-      visibilityObserver = observeElementVisibility(
-        container.value,
-        scene.setViewVisible,
-      );
 
       resize();
       registerView(registryKeys, viewApi);
@@ -132,7 +126,6 @@ export default {
     });
 
     onBeforeUnmount(() => {
-      visibilityObserver?.dispose();
       unregisterView(registryKeys, viewApi);
       scene.cleanup();
 
